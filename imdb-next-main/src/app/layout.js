8d18fc3c562fe3c7a0027632670d58f9f101ccae
Payal Ramkrishna Dhobale <143871737+Payal234@users.noreply.github.com>
2024-08-26 08:38:45 +0530

    Add files via upload

---
+++ imdb-next-main/src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
